24587bfb52698828d452dffa788171d384fb21ea
Axel-al <axel.albtrx@gmail.com>
2025-05-31 18:20:51 +0200

    Implement Coordonnees class with accessors, toString, equals, and hashCode methods

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/Coordonnees.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/Coordonnees.java
@@ -1,4 +1,48 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
 public class Coordonnees {
+    private int abscisse;
+    private int ordonnee;
+
+    public Coordonnees(int abscisse, int ordonnee) {
+        this.abscisse = abscisse;
+        this.ordonnee = ordonnee;
+    }
+
+    public int getAbscisse() {
+        return abscisse;
+    }
+
+    public void setAbscisse(int abscisse) {
+        this.abscisse = abscisse;
+    }
+
+    public int getOrdonnee() {
+        return ordonnee;
+    }
+
+    public void setOrdonnee(int ordonnee) {
+        this.ordonnee = ordonnee;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + abscisse + ", " + ordonnee + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordonnees that = (Coordonnees) o;
+        return getAbscisse() == that.getAbscisse() && getOrdonnee() == that.getOrdonnee();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getAbscisse();
+        result = 31 * result + getOrdonnee();
+        return result;
+    }
 }
