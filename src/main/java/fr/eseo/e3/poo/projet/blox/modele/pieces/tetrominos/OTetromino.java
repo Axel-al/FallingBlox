c6a5f6b19dc5fc3945d3f8e3d4feccb1992c4fa0
Axel-al <axel.albtrx@gmail.com>
2025-06-01 01:26:35 +0200

    feat: implement OTetromino with constructor, element initialization, and toString method

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/OTetromino.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/OTetromino.java
@@ -1,4 +1,35 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos;
 
+import fr.eseo.e3.poo.projet.blox.modele.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class OTetromino extends Tetromino {
+    private final Element[] elements;
+
+    public OTetromino(Coordonnees coordonnees, Couleur couleur) {
+        super();
+        this.elements = new Element[4];
+        setElements(coordonnees, couleur);
+    }
+
+    protected void setElements(Coordonnees coordonnees, Couleur couleur) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                int abscisse = coordonnees.getAbscisse() - i;
+                int ordonnee = coordonnees.getOrdonnee() + j;
+                this.elements[i*2 + j] = new Element(new Coordonnees(abscisse, ordonnee), couleur);
+            }
+        }
+    }
+
+    public Element[] getElements() {
+        return elements;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " :\n\t" +
+                Arrays.stream(elements).map(Element::toString).collect(Collectors.joining("\n\t"));
+    }
 }
