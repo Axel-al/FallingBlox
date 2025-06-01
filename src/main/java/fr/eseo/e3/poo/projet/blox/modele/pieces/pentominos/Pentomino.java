d4c6b68d96189050e94cbf68b74916f037aa91ef
Axel-al <axel.albtrx@gmail.com>
2025-06-01 16:00:46 +0200

    feat: implement abstract Pentomino with element management and positioning logic

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/pentominos/Pentomino.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/pentominos/Pentomino.java
@@ -1,6 +1,34 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.pentominos;
 
+import fr.eseo.e3.poo.projet.blox.modele.*;
 import fr.eseo.e3.poo.projet.blox.modele.pieces.*;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public abstract class Pentomino implements Piece {
+    protected Element[] elements;
+
+    public Pentomino(Coordonnees coordonnees, Couleur couleur) {
+        this.elements = new Element[5];
+        setElements(coordonnees, couleur);
+    }
+
+    protected abstract void setElements(Coordonnees coordonnees, Couleur couleur);
+
+    @Override
+    public Element[] getElements() {
+        return elements;
+    }
+
+    @Override
+    public void setPosition(int abscisse, int ordonnee) {
+        setElements(new Coordonnees(abscisse, ordonnee), getCouleur());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " :\n\t" +
+                Arrays.stream(elements).map(Element::toString).collect(Collectors.joining("\n\t"));
+    }
 }
