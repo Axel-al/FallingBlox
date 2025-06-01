8e9e067cee9efc7498dd34bc15890d2c040eb1e9
Axel-al <axel.albtrx@gmail.com>
2025-06-01 17:32:20 +0200

    feat: implement all Pentomino shapes with constructors and setElements logic

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/pentominos/YPentomino2.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/pentominos/YPentomino2.java
@@ -1,4 +1,20 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.pentominos;
 
+import fr.eseo.e3.poo.projet.blox.modele.*;
+
 public class YPentomino2 extends Pentomino {
+    public YPentomino2(Coordonnees coordonnees, Couleur couleur) {
+        super(coordonnees, couleur);
+    }
+
+    @Override
+    protected void setElements(Coordonnees coordonnees, Couleur couleur) {
+        int abscisse = coordonnees.getAbscisse();
+        int ordonnee = coordonnees.getOrdonnee();
+        this.elements[0] = new Element(coordonnees, couleur);
+        this.elements[1] = new Element(new Coordonnees(abscisse + 1, ordonnee), couleur);
+        this.elements[2] = new Element(new Coordonnees(abscisse, ordonnee - 1), couleur);
+        this.elements[3] = new Element(new Coordonnees(abscisse, ordonnee + 1), couleur);
+        this.elements[4] = new Element(new Coordonnees(abscisse, ordonnee + 2), couleur);
+    }
 }
--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/pentominos/ZPentomino1.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/pentominos/ZPentomino1.java
@@ -1,4 +1,20 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.pentominos;
 
+import fr.eseo.e3.poo.projet.blox.modele.*;
+
 public class ZPentomino1 extends Pentomino {
+    public ZPentomino1(Coordonnees coordonnees, Couleur couleur) {
+        super(coordonnees, couleur);
+    }
+
+    @Override
+    protected void setElements(Coordonnees coordonnees, Couleur couleur) {
+        int abscisse = coordonnees.getAbscisse();
+        int ordonnee = coordonnees.getOrdonnee();
+        this.elements[0] = new Element(coordonnees, couleur);
+        this.elements[1] = new Element(new Coordonnees(abscisse, ordonnee - 1), couleur);
+        this.elements[2] = new Element(new Coordonnees(abscisse - 1, ordonnee - 1), couleur);
+        this.elements[3] = new Element(new Coordonnees(abscisse, ordonnee + 1), couleur);
+        this.elements[4] = new Element(new Coordonnees(abscisse + 1, ordonnee + 1), couleur);
+    }
 }
