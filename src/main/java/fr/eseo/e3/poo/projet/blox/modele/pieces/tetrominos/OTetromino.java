d54cc8062a2e9737d00b0ca57d91ea9cdb633fbf
Axel-al <axel.albtrx@gmail.com>
2025-06-01 11:36:36 +0200

    refactor: simplify OTetromino by leveraging Tetromino base implementation

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/OTetromino.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/OTetromino.java
@@ -1,35 +1,20 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos;
 
 import fr.eseo.e3.poo.projet.blox.modele.*;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class OTetromino extends Tetromino {
-    private final Element[] elements;
-
     public OTetromino(Coordonnees coordonnees, Couleur couleur) {
-        super();
-        this.elements = new Element[4];
-        setElements(coordonnees, couleur);
-    }
-
-    protected void setElements(Coordonnees coordonnees, Couleur couleur) {
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                int abscisse = coordonnees.getAbscisse() + i;
-                int ordonnee = coordonnees.getOrdonnee() - j;
-                this.elements[i*2 + j] = new Element(new Coordonnees(abscisse, ordonnee), couleur);
-            }
-        }
-    }
-
-    public Element[] getElements() {
-        return elements;
+        super(coordonnees, couleur);
     }
 
     @Override
-    public String toString() {
-        return this.getClass().getSimpleName() + " :\n\t" +
-                Arrays.stream(elements).map(Element::toString).collect(Collectors.joining("\n\t"));
+    protected void setElements(Coordonnees coordonnees, Couleur couleur) {
+        int abscisse = coordonnees.getAbscisse();
+        int ordonnee = coordonnees.getOrdonnee();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                this.elements[i*2 + j] = new Element(new Coordonnees(abscisse + j, ordonnee - i), couleur);
+            }
+        }
     }
 }
