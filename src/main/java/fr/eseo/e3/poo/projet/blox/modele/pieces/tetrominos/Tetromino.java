adc037d66e264e7c22cdd1f2fd68f5a172a66bf6
Axel-al <axel.albtrx@gmail.com>
2025-06-01 11:36:01 +0200

    feat: implement abstract Tetromino with element management and position setting

--- src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/Tetromino.java
+++ src/main/java/fr/eseo/e3/poo/projet/blox/modele/pieces/tetrominos/Tetromino.java
@@ -1,6 +1,34 @@
 package fr.eseo.e3.poo.projet.blox.modele.pieces.tetrominos;
 
+import fr.eseo.e3.poo.projet.blox.modele.*;
 import fr.eseo.e3.poo.projet.blox.modele.pieces.*;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public abstract class Tetromino implements Piece {
+    protected final Element[] elements;
+
+    public Tetromino(Coordonnees coordonnees, Couleur couleur) {
+        this.elements = new Element[4];
+        setElements(coordonnees, couleur);
+    }
+
+    protected abstract void setElements(Coordonnees coordonnees, Couleur couleur);
+
+    @Override
+    public Element[] getElements() {
+        return elements;
+    }
+
+    @Override
+    public void setPosition(int abscisse, int ordonnee) {
+        setElements(new Coordonnees(abscisse, ordonnee), getCouleur());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " :\n\t" +
+                Arrays.stream(elements).map(Element::toString).collect(Collectors.joining("\n\t"));
+    }
 }
