5722be6eaeb908a7a5b2b430628995eed3f7ea06
Axel Alabeatrix <axel.alabeatrix@reseau.eseo.fr>
2025-06-02 10:34:01 +0200

    test: add PieceRotationTest to verify piece rotation on left and right mouse click

---
+++ src/test/java/fr/eseo/e3/poo/projet/blox/controleur/PieceRotationTest.java
@@ -0,0 +1,34 @@
+package fr.eseo.e3.poo.projet.blox.controleur;
+
+import fr.eseo.e3.poo.projet.blox.modele.Puits;
+import fr.eseo.e3.poo.projet.blox.modele.pieces.UsineDePiece;
+import fr.eseo.e3.poo.projet.blox.vue.VuePuits;
+
+import javax.swing.*;
+
+public class PieceRotationTest {
+
+    public PieceRotationTest() {
+        creerFenetreTest();
+    }
+
+    private void creerFenetreTest() {
+        Puits puits = new Puits();
+        VuePuits vuePuits = new VuePuits(puits, 40);
+
+        // Génère une pièce de test visible
+        puits.setPieceSuivante(UsineDePiece.genererTetromino());
+        puits.setPieceSuivante(UsineDePiece.genererTetromino());
+
+        JFrame frame = new JFrame("Test rotation souris");
+        frame.setContentPane(vuePuits);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(PieceRotationTest::new);
+    }
+}
